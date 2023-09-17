eaef95828eec9ba9881e1e2ee93d89a9978c9292
Carlos <carlos.bermudez01@usa.edu.co>
2023-09-17 02:27:12 -0500

    Modificar diseño view

--- src/view/PanoramicView.java
+++ src/view/PanoramicView.java
@@ -35,13 +35,13 @@ public class PanoramicView extends JFrame {
 
     private void createButtonPanel(JPanel mainPanel) {
         // Panel para los botones en la parte inferior
-        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 0)); // Establecer espacios horizontal y vertical a 0
         mainPanel.add(buttonPanel, BorderLayout.SOUTH);
 
-        JButton btnArriba = createButton("Arriba");
-        JButton btnAbajo = createButton("Abajo");
-        JButton btnIzquierda = createButton("Izquierda");
-        JButton btnDerecha = createButton("Derecha");
+        JButton btnArriba = createButton("\u2191");
+        JButton btnAbajo = createButton("\u2193");
+        JButton btnIzquierda = createButton("\u2190");
+        JButton btnDerecha = createButton("\u2192");
 
         buttonPanel.add(btnArriba);
         buttonPanel.add(btnAbajo);
@@ -50,10 +50,18 @@ public class PanoramicView extends JFrame {
     }
 
     private JButton createButton(String text) {
+        Font buttonFont = new Font("Arial", Font.PLAIN, 30); // Cambia "Arial" y 24 al tamaño deseado
+
         // Crear botones con fondo blanco y texto negro
         JButton button = new JButton(text);
         button.setBackground(Color.WHITE);
         button.setForeground(Color.BLACK);
+
+        button.setFont(buttonFont); // Establecer la fuente personalizada
+
+        // Establecer el tamaño del botón
+        button.setPreferredSize(new Dimension(70, 50)); // Cambia el tamaño según tus necesidades
+
         return button;
     }
 
